80136cac5da7fb3308c7420be7748003858fd557
ncqwer <1732031236@qq.com>
2024-02-22 14:44:12 +0800

    feat: 不知道干嘛

--- script/changelog.js
+++ script/changelog.js
@@ -5,7 +5,7 @@ const merge = require('lodash.merge');
 
 // 使用git命令获取提交信息，使用指定的格式化字符串输出
 
-const titleReg = /^(\S+?)(?:\((\S+)\))?\s*[\:\：]\s*(\S.*)/g;
+const titleReg = /^(\S+?)(?:\((\S+)\))?\s*[\:\：](.*)/g;
 const breakchangeReg = /^BREAKING CHANGE\s*[\:\：]\s*(\S.*)/g;
 
 const main = async () => {
@@ -26,18 +26,27 @@ const main = async () => {
     );
   });
   const tmpResult = {};
+  const revertCommitMap = {};
   commitsRaw.split('===').forEach((commitStr) => {
     if (!commitStr) return;
     const lines = commitStr.split('\n');
     const [hash, shotHash, authorName, authorEmail] = lines[1].split(';');
+    if (revertCommitMap[hash]) {
+      // current commit has been reverted
+      return;
+    }
     lines.slice(2).forEach((_line) => {
       const line = _line.trim();
       const titleResult = titleReg.exec(line);
-      const breakChangeResult = !titleResult ? breakchangeReg.exec(line) : null;
+      const breakChangeResult = breakchangeReg.exec(line);
+      let library_name = 'library_name from branch';
       if (titleResult) {
         const type = titleResult[1];
-        const library_name = titleResult[2];
+        library_name = titleResult[2];
         const subject = titleResult[3];
+        if (type === 'revert') {
+          revertCommitMap[library_name] = true;
+        }
         if (isValidType(type) && !!library_name) {
           merge(tmpResult, {
             [library_name]: {
