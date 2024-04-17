cc6edeab118b25a5ff4a7b66c0c6ff0586db7324
ncqwer <1732031236@qq.com>
2024-04-17 19:54:23 +0800

    why

--- script/temp/success.js
+++ script/temp/success.js
@@ -2,8 +2,13 @@ const { GITHUB_REPOSITORY, ACTION_ID } = require('./env');
 const { sendMessage } = require('./error');
 
 const main = async () => {
-  const url = `https://github.com/${GITHUB_REPOSITORY}/actions/runs/${ACTION_ID}`;
-  await sendMessage(`[点击此处获得编译产物](${url})`);
+  try {
+    const url = `https://github.com/${GITHUB_REPOSITORY}/actions/runs/${ACTION_ID}`;
+    const data = await sendMessage(`[点击此处获得编译产物](${url})`);
+    console.log('🚀 ~ file: success.js:9 ~ main ~ data:', data);
+  } catch (e) {
+    console.log('🚀 ~ file: success.js:10 ~ main ~ e:', e);
+  }
 };
 
 main();
